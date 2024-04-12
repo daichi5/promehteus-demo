b54958ed6009eb89985c7fcfe7b595c1fa162c9a
daichi5 <sawa22daichi@gmail.com>
2024-04-13 01:42:29 +0900

    forward from nginx to api server

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,7 @@ import fastify from 'fastify'
 
 const server = fastify()
 
-server.get('/ping', async (request, reply) => {
+server.get('/api/ping', async (request, reply) => {
   return 'pong\n'
 })
 
